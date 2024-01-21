fed6d51a83bdb6de432f9ea893528be5b3cadbbb
Jerry Zheng <jerryzhengpro@gmail.com>
2024-01-20 18:55:18 -0800

    Improve nav (#2)

---
+++ pages/CreateScreen.js
@@ -0,0 +1,5 @@
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function CreateScreen({ navigation, route }) {
+  return <Text>Create Screen</Text>;
+};
---
+++ pages/PlayScreen.js
@@ -0,0 +1,5 @@
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function PlayScreen({ navigation, route }) {
+  return <Text>Play Screen</Text>;
+};
